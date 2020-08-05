a337dbfbb4423c1756d06cda0638e58b49be3f7f
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-05 12:32:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,7 +24,7 @@ end
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :winter
-      holiday.each do |key, supplies|
+      holiday.each do |supplies, supplies|
         
     binding.pry
 
